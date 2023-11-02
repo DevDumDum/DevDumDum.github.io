d688fda7f35005a3f421f35b3d97bb5c8fa20acd
Maku Aren <markallencabutaje00@gmail.com>
2023-11-03 01:54:34 +0800

    Featured Project Polish

--- js/slide.js
+++ js/slide.js
@@ -1,5 +1,6 @@
 $(function() {
-  
+    var counter = 0;
+    
     var slideCount =  $(".slider ul li").length;
     var slideWidth =  $(".slider ul li").width();
     var slideHeight =  $(".slider ul li").height();
@@ -8,7 +9,7 @@ $(function() {
     $(".slider").css({"max-width":slideWidth, "height": slideHeight});
     $(".slider ul").css({"width":slideUlWidth, "margin-left": - slideWidth });
     $(".slider ul li:last-child").prependTo($(".slider ul"));
-
+    document.getElementById(counter).style.color = 'rgb(255, 215, 95)';
 
     function moveLeft() {
       $(".slider ul").stop().animate({
@@ -26,7 +27,20 @@ $(function() {
         $(".slider ul li:first-child").appendTo($(".slider ul"));
         $(".slider ul").css("left","");
       });
-      setTimeout(moveRight, 3000);
+
+        
+        if(counter < slideCount-1){
+            counter++;
+        } else {
+            counter = 0;
+        }
+    if(counter-1 >= 0){
+        document.getElementById(counter-1).style.color = 'rgb(220,220,220)';
+    } else {
+        document.getElementById(slideCount-1).style.color = 'rgb(220,220,220)';
+    }
+    document.getElementById(counter).style.color = 'rgb(255, 215, 95)';
+    setTimeout(moveRight, 3000);
     }
     
     
@@ -38,5 +52,5 @@ $(function() {
       moveLeft();
     });
     
-    // setTimeout(moveRight, 3000);    
+    setTimeout(moveRight, 3000);    
   });
